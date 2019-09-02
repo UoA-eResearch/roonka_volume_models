cbdbc0b7f2a5cba620a9131765389913c918f9ce
warrick <wcor690@aucklanduni.ac.nz>
2019-09-03 11:46:11 +1200

    Creating bounding box of correct size and scale.

--- trench_wall.py
+++ trench_wall.py
@@ -24,19 +24,30 @@ def create_bounding_box():
     # just make a cube of min -> max values on every axis.
     # TODO: quite easy to sort scaling because cube scale = 1 unit either direction.
     # e.g. 15 on x axis covers -15, 15. Just sort by math.abs(vert.co.x, y, and z then do accordingly)
-    print("run")
+    print("creating bounding box")
     obj = bpy.context.object
     me = bpy.context.object.data
     edit_mode()
     bm = bmesh.from_edit_mesh(me)
     bm.faces.ensure_lookup_table()
     bm.verts.ensure_lookup_table()
-    start_pos = bm.faces[0].verts[0].co + obj.location
+    bounding_box_center = obj.location
+    
+    # TODO: refactor into singular function
+    max_x_position = sorted(bm.verts, reverse=True, key=lambda v: abs(v.co.x))
+    max_x = abs(max_x_position[0].co.x)
+
+    max_y_position = sorted(bm.verts, reverse=True, key=lambda v: abs(v.co.y))
+    max_y = abs(max_y_position[0].co.y)
+
+    max_z_position = sorted(bm.verts, reverse=True, key=lambda v: abs(v.co.z))
+    max_z = abs(max_z_position[0].co.z)
     obj_mode()
-#     bpy.ops.mesh.primitive_cube_add(location=start_pos)
-    bounding_box = bpy.ops.mesh.primitive_cube_add(location=(0, 0, 0))
-    bpy.context.scene.objects.active.scale = (2.2, 1, 1)
-    # start expanding cube till no intersects or contains all points.
+    
+    bpy.ops.mesh.primitive_cube_add(location=bounding_box_center)
+    bpy.context.scene.objects.active.scale = (max_x, max_y, max_z)
+
+    
     
 
 def bridge_all_loops():
