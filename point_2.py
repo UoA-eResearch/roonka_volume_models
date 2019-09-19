153a8b05742e051e2b4435adb874b21ec8273a48
warrick <wcor690@aucklanduni.ac.nz>
2019-09-20 10:56:25 +1200

    Use both inside functions to reduce false positives for vertices inside features.

--- point_2.py
+++ point_2.py
@@ -5,6 +5,7 @@ import bmesh
 from mathutils import Vector, Matrix
 from math import pi, acos
 
+
 def edit_mode():
         bpy.ops.object.mode_set(mode='EDIT')
 
@@ -83,8 +84,9 @@ for ob in objects:
     if ob.name.startswith('Artefacts'):
         start_pos = ob.location
         end_pos = start_pos + Vector([0, 0, 1000])
-        if is_inside2(ob.location, volume_obj):
+        # if is_inside2(ob.location, volume_obj):
         # if is_inside(start_pos, end_pos, volume_obj):
+        if is_inside(start_pos, end_pos, volume_obj) and is_inside2(ob.location, volume_obj):
             ob.select = True
             count += 1
             print(ob.name)
