8d023b9de7d4d98b420b2868b315f3199de68b60
warrick <wcor690@aucklanduni.ac.nz>
2019-09-19 15:31:32 +1200

    Added alternative is_inside method. Seems to be more accurate in some instances.

--- point_2.py
+++ point_2.py
@@ -4,7 +4,7 @@ import bpy
 import bmesh
 from mathutils import Vector, Matrix
 
-
+from math import pi, acos
 
 
 def edit_mode():
@@ -94,6 +94,7 @@ def is_inside(ray_origin, ray_destination, obj):
 
     max_expected_intersections = 1000
     fudge_distance = 0.0001
+    fudge_distance = 3.1
     direction = (ray_destination - loc)
     dir_len = direction.length
     amount = fudge_distance / dir_len
@@ -115,9 +116,10 @@ def is_inside(ray_origin, ray_destination, obj):
 # artefacts now active
 active_obj = objects['Artefacts']
 art_offset = active_obj.location
-select('Artefacts')
+# select('Artefacts')
 art_verts = active_obj.data.vertices
 count =0
+
 
 # for vert in art_verts:
 #     start_pos = art_offset + vert.co
@@ -130,12 +132,32 @@ count =0
 
 # TODO: iterate all separated artefact points and select based on is_inside():
 
+
+def is_inside2(target_pt_global, mesh_obj, tolerance=0.11):
+    '''
+    Method using comparing of outward facing mesh normal with vertex point.
+    '''
+    # Convert the point from global space to mesh local space
+    target_pt_local = mesh_obj.matrix_world.inverted() * target_pt_global
+    # Find the nearest point on the mesh and the nearest face normal
+    _, pt_closest, face_normal, _ = mesh_obj.closest_point_on_mesh(target_pt_local)
+    # Get the target-closest pt vector
+    target_closest_pt_vec = (pt_closest - target_pt_local).normalized()
+    # Compute the dot product = |a||b|*cos(angle)
+    dot_prod = target_closest_pt_vec.dot(face_normal)
+    # Get the angle between the normal and the target-closest-pt vector (from the dot prod)
+    angle = acos(min(max(dot_prod, -1), 1)) * 180 / pi
+    # Allow for some rounding error
+    inside = angle < 90-tolerance
+    return inside
+
 for ob in objects:
     if ob.name.startswith('Artefacts'):
         # print(ob.name)
         start_pos = ob.location
-        end_pos = start_pos + Vector([0, 0, 100])
-        if is_inside(start_pos, end_pos, volume_obj):
+        end_pos = start_pos + Vector([0, 0, 1000])
+        if is_inside2(ob.location, volume_obj):
+        # if is_inside(start_pos, end_pos, volume_obj):
             ob.select = True
             count += 1
             print(ob.name)
