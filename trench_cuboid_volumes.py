edb44265b56c29c0f09beed95ea089723f6e2b91
warrick <wcor690@aucklanduni.ac.nz>
2019-09-30 11:07:42 +1300

    Creating rectangular layer volumes.

--- trench_cuboid_volumes.py
+++ trench_cuboid_volumes.py
@@ -3,6 +3,8 @@ import bpy
 import bmesh
 from mathutils import Vector, Matrix
 
+remesh_octree_depth = 6
+
 def edit_mode():
     bpy.ops.object.mode_set(mode='EDIT')
 
@@ -24,6 +26,17 @@ def end():
     print(10/0)
 
 
+def deselect_all():
+    bpy.ops.object.select_all(action='DESELECT')
+
+
+def select_objects(str):
+    # objects = []
+    for obj in bpy.data.objects:
+        if obj.name.startswith('TIN'):
+            obj.select = True
+
+
 data = bpy.data
 objects = bpy.data.objects
 active_obj = bpy.context.scene.objects.active
@@ -39,6 +52,10 @@ for layer in layers:
     active_obj = layer
     bpy.ops.tesselation.delaunay()
 
+# Joining delaunay layers
+print('attempting join...')
+deselect_all()
+select_objects('TIN')
 bpy.ops.object.join()
 join_layer_name = bpy.context.scene.objects.active.name
 print(join_layer_name)
@@ -49,24 +66,18 @@ print(join_layer_name)
 cube_spawn_loc /= 2
 bpy.ops.mesh.primitive_cube_add(location=cube_spawn_loc)
 
-# not the same reference
-print(active_obj.name)
-print(bpy.context.scene.objects.active)
-# end()
-
 #Remesh
 remesher = bpy.context.scene.objects.active.modifiers.new(name="Remesh", type="REMESH")
-remesher.octree_depth = 6
+remesher.octree_depth = remesh_octree_depth
 remesher.mode = "BLOCKS"
 remesher.use_smooth_shade = False
 bpy.ops.object.modifier_apply(apply_as='DATA', modifier="Remesh")
 
 # Shrinkwrap
-
-# shrinker = obj.modifiers.new(name="Shrinkwrap", type="SHRINKWRAP")
-# shrinker.target = bpy.data.objects[original.name]
-# # shrinker.wrap_method = 'NEAREST_VERTEX'
+shrinker = bpy.context.scene.objects.active.modifiers.new(name="Shrinkwrap", type="SHRINKWRAP")
+shrinker.target = bpy.data.objects[join_layer_name]
+shrinker.wrap_method = 'NEAREST_VERTEX'
 # shrinker.wrap_method = shrink_method
-# bpy.ops.object.modifier_apply(apply_as='DATA', modifier="Shrinkwrap")
+bpy.ops.object.modifier_apply(apply_as='DATA', modifier="Shrinkwrap")
 
 # Decimate/Subdivide
